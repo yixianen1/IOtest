645609f8365a000f69454b27a0268dd299430d51
yixiane <yixianenyouxiang@163.com>
2019-10-09 20:14:38 +0800

    java中级-IO流

--- src/com/yixianen/www/io1.java
+++ src/com/yixianen/www/io1.java
@@ -5,19 +5,19 @@ import java.io.*;
 public class io1 {
     public static void main(String[] args) {
         /*字节流读取文件内容*/
-//        try {
-//            File f = new File("E:/lol.txt");
-//            //创建基于文件的输入流
-//            FileInputStream fis = new FileInputStream(f);
-//            byte [] all = new byte[(int) f.length()];
-//            fis.read(all);
-//            for(byte b:all){
-//                System.out.println(b);
-//            }
-//            fis.close();
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
+        try {
+            File f = new File("E:/lol.txt");
+            //创建基于文件的输入流
+            FileInputStream fis = new FileInputStream(f);
+            byte [] all = new byte[(int) f.length()];
+            fis.read(all);
+            for(byte b:all){
+                System.out.println(b);
+            }
+            fis.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
         /*字节流读取文件内容*/
         try {
             File file = new File("E:/lol2.txt");
